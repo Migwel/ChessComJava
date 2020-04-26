a44c9301c41421568bef21475779280c2e7f8b35
migwel <migonzalez1991@gmail.com>
2020-04-26 21:02:04 +0200

    Do proper testing for PlayerService

--- chesscom/src/test/java/dev/migwel/chesscomjava/implementation/PlayerServiceImplTest.java
+++ chesscom/src/test/java/dev/migwel/chesscomjava/implementation/PlayerServiceImplTest.java
@@ -5,38 +5,64 @@ import dev.migwel.chesscomjava.api.player.Player;
 import dev.migwel.chesscomjava.api.player.PlayerStats;
 import dev.migwel.chesscomjava.api.player.TitledPlayers;
 import dev.migwel.chesscomjava.services.PlayerService;
+import dev.migwel.chesscomjava.util.FileUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PlayerServiceImplTest {
 
-    private final HttpClient httpClient = new HttpClient();
+    private final HttpClient httpClient = mock(HttpClient.class);
     private final ChessComFetcher chessComFetcher = new ChessComFetcher(httpClient);
     private final PlayerService playerService = new PlayerServiceImpl(chessComFetcher);
 
+    @BeforeEach
+    void before() throws URISyntaxException, IOException {
+        String profile = FileUtil.loadFile("Profile.json");
+        String titled = FileUtil.loadFile("Titled.json");
+        String playerStats = FileUtil.loadFile("PlayerStats.json");
+        String isOnline = FileUtil.loadFile("IsOnline.json");
+        when(httpClient.httpGet(contains("player"))).thenReturn(profile);
+        when(httpClient.httpGet(contains("titled"))).thenReturn(titled);
+        when(httpClient.httpGet(contains("stats"))).thenReturn(playerStats);
+        when(httpClient.httpGet(contains("online"))).thenReturn(isOnline);
+    }
+
     @Test
     void testGetProfile() {
-        Player player = playerService.getProfile("mig_well");
-        assertEquals(player.username(), "mig_well");
+        Player player = playerService.getProfile("erik");
+        assertEquals(player.username(), "erik");
+        assertEquals(player.avatar(), "https://images.chesscomfiles.com/uploads/v1/user/41.f4cbdaf7.200x200o.2da5fc98b62f.jpeg");
+        assertEquals(player.id(), 41);
+        assertFalse(player.isStreamer());
     }
 
     @Test
     void testGetTitledPlayers() {
         TitledPlayers titledPlayers = playerService.getTitledPlayers("GM");
-        assertEquals(titledPlayers.players().size(), 1172);
+        assertEquals(titledPlayers.players().size(), 1173);
+        String titledPlayer = titledPlayers.players().iterator().next();
+        assertEquals(titledPlayer, "124chess");
     }
 
     @Test
     void testGetPlayerStats() {
-        PlayerStats playerStats = playerService.getPlayerStats("mig_well");
-        assertEquals(playerStats.fide(), 0);
+        PlayerStats playerStats = playerService.getPlayerStats("erik");
+        assertEquals(playerStats.fide(), 1707);
+        assertEquals(playerStats.rapidChessResults().best().game(), "https://www.chess.com/live/game/758005361");
+        assertEquals(playerStats.puzzleRushResults().best().score(), 40);
     }
 
     @Test
     void testIsOnline() {
-        boolean isOnline = playerService.isOnline("mig_well");
+        boolean isOnline = playerService.isOnline("erik");
         assertTrue(isOnline);
     }
 }
