8b49b7a310951f916fbc19401c7dcfd70b309e5e
migwel <migonzalez1991@gmail.com>
2020-04-26 18:01:07 +0200

    Do proper testing for StreamerService (more to come)

--- chesscom/src/test/java/dev/migwel/chesscomjava/implementation/StreamerServiceImplTest.java
+++ chesscom/src/test/java/dev/migwel/chesscomjava/implementation/StreamerServiceImplTest.java
@@ -1,20 +1,42 @@
 package dev.migwel.chesscomjava.implementation;
 
 import dev.migwel.chesscomjava.ChessComFetcher;
+import dev.migwel.chesscomjava.api.streamer.Streamer;
 import dev.migwel.chesscomjava.api.streamer.Streamers;
 import dev.migwel.chesscomjava.services.StreamerService;
+import dev.migwel.chesscomjava.util.FileUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.*;
+
 
 class StreamerServiceImplTest {
 
-    private final HttpClient httpClient = new HttpClient();
+    private final HttpClient httpClient = mock(HttpClient.class);
     private final ChessComFetcher chessComFetcher = new ChessComFetcher(httpClient);
     private final StreamerService streamerService = new StreamerServiceImpl(chessComFetcher);
+
+    @BeforeEach
+    void before() throws URISyntaxException, IOException {
+        String streamersJson = FileUtil.loadFile("Streamers.json");
+        when(httpClient.httpGet(contains("https://api.chess.com/pub/streamers"))).thenReturn(streamersJson);
+    }
 
     @Test
     void getStreamers() {
         Streamers streamers = streamerService.getStreamers();
-        System.out.println(streamers);
+        assertEquals(streamers.streamers().size(), 240);
+        Streamer streamer = streamers.streamers().iterator().next();
+        assertEquals(streamer.avatar(), "https://images.chesscomfiles.com/uploads/v1/user/32076054.611f6b88.50x50o.b34120ef650c.jpeg");
+        assertEquals(streamer.twitchUrl(), "https://twitch.tv/GMBenjaminFinegold");
+        assertEquals(streamer.url(), "https://www.chess.com/member/GMBenjaminFinegold");
+        assertEquals(streamer.username(), "GMBenjaminFinegold");
     }
 
 }
