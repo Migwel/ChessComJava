9b2037cfc4749fa96dd5586b8e53b29ec4e1fb03
migwel <migonzalez1991@gmail.com>
2020-05-02 15:49:30 +0200

    Add handling of "wrong" http codes

--- implementation/src/main/java/dev/migwel/chesscomjava/HttpResponseHandler.java
+++ implementation/src/main/java/dev/migwel/chesscomjava/HttpResponseHandler.java
@@ -10,8 +10,21 @@ import java.io.IOException;
 
 public class HttpResponseHandler implements ResponseHandler<String> {
     @Override
-    public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+    public String handleResponse(HttpResponse response) throws IOException {
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode < 200 || statusCode > 304) {
+            handleWrongStatusCode(statusCode);
+        }
         HttpEntity entity = response.getEntity();
         return EntityUtils.toString(entity);
+    }
+
+    private void handleWrongStatusCode(int statusCode) throws ClientProtocolException {
+        switch (statusCode) {
+            case 404 -> throw new ClientProtocolException("Resource could not be found");
+            case 410 -> throw new ClientProtocolException("The resource is no longer available at that URL");
+            case 429 -> throw new ClientProtocolException("Too many requests. We are being rate limited");
+        }
+        throw new ClientProtocolException("Something went wrong with Http communication. Code: "+ statusCode);
     }
 }
---
+++ implementation/src/test/java/dev/migwel/chesscomjava/HttpResponseHandlerTest.java
@@ -0,0 +1,70 @@
+package dev.migwel.chesscomjava;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class HttpResponseHandlerTest {
+
+    HttpResponseHandler responseHandler = new HttpResponseHandler();
+
+    @Test
+    void test400StatusCode() throws IOException {
+        HttpResponse response = getHttpResponse(400);
+        try {
+            responseHandler.handleResponse(response);
+            fail();
+        } catch (ClientProtocolException e) {
+            assertEquals("Something went wrong with Http communication. Code: 400", e.getMessage());
+        }
+    }
+
+    @Test
+    void test404StatusCode() throws IOException {
+        HttpResponse response = getHttpResponse(404);
+        try {
+            responseHandler.handleResponse(response);
+            fail();
+        } catch (ClientProtocolException e) {
+            assertEquals("Resource could not be found", e.getMessage());
+        }
+    }
+
+    @Test
+    void test410StatusCode() throws IOException {
+        HttpResponse response = getHttpResponse(410);
+        try {
+            responseHandler.handleResponse(response);
+            fail();
+        } catch (ClientProtocolException e) {
+            assertEquals("The resource is no longer available at that URL", e.getMessage());
+        }
+    }
+
+    @Test
+    void test429StatusCode() throws IOException {
+        HttpResponse response = getHttpResponse(429);
+        try {
+            responseHandler.handleResponse(response);
+            fail();
+        } catch (ClientProtocolException e) {
+            assertEquals("Too many requests. We are being rate limited", e.getMessage());
+        }
+    }
+
+    private HttpResponse getHttpResponse(int statusCode) {
+        HttpResponse response = mock(HttpResponse.class);
+        StatusLine statusLine = mock(StatusLine.class);
+        when(response.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(statusCode);
+        return response;
+    }
+
+}
