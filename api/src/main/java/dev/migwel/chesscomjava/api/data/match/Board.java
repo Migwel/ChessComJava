153787c41ce79b03338ffe40a3ebf7d1c9865c4e
migwel <migonzalez1991@gmail.com>
2020-05-02 15:07:21 +0200

    Deal properly with varying keys for boardScores (easier than I thought)

--- api/src/main/java/dev/migwel/chesscomjava/api/data/match/Board.java
+++ api/src/main/java/dev/migwel/chesscomjava/api/data/match/Board.java
@@ -3,7 +3,8 @@ package dev.migwel.chesscomjava.api.data.match;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Collection;
+import java.util.Map;
 
-public record Board(@SerializedName("board_scores") BoardScores boardScores,
+public record Board(@SerializedName("board_scores") Map<String, Long> boardScores,
                     Collection<BoardGame> games)
 {}
--- api/src/main/java/dev/migwel/chesscomjava/api/data/match/BoardScores.java
+++
@@ -1,7 +0,0 @@
-package dev.migwel.chesscomjava.api.data.match;
-
-public record BoardScores(
-        //Empty for now. This has dynamic keys
-        //TODO
-)
-{}
